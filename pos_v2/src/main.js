16c40774b96ccb3c003aae4219e2033ad112fb8b
wanghuan <654296217@qq.com>
2014-08-23 10:54:45 +0800

    all functions are ok

--- pos_v2/src/main.js
+++ pos_v2/src/main.js
@@ -1,40 +1,33 @@
 $(document).ready(function(){
 
-    hideButton();
     var answerGenerate = new GenerateNum();
     var compareNum = new CompareNumber();
     var guess = new Guess(answerGenerate, compareNum);
     var game = new Interface(guess);
 
+    hideButton();
+
     $('.startButton').on('click',start(game));
+    // $('.submitButton').on('click', processAnswer(game));
+
     $('.submitButton').on('click', function(){
 
-            var input, judgeInput;
+        var input, judgeInput;
 
-            input = $('.inputBox')[0].value;
-            judgeInput = game.judgeInput(input);
-            if(!judgeInput){
-                alert('请输入四位不相同的数字');
+        input = $('.inputBox')[0].value;
+        judgeInput = game.judgeInput(input);
+
+        if(!judgeInput){
+            alert('请输入四位不相同的数字');
+        }else{
+            var judgeAnswerString = game.judgeAnswer(input);
+            if(judgeAnswerString === 'Congratulation !'){
+                succeedProcess(judgeAnswerString);
             }else{
-                var judgeAnswerString = game.judgeAnswer(input);
-                if(judgeAnswerString === 'Congratulation !'){
-                    $('.congratulation').text(judgeAnswerString);
-                    hideButton();
-                }else{
-                    var number = game.modifyNumber();
-                    if(number != 'Game over !'){
-                        alert('你的答案是:' + judgeAnswerString +'你还有' + number +'次机会!');
-                        $('.chanceNum').text(number);
-                    }else{
-                        alert(number);
-                        $('.chanceNum').text(0);
-                        $('.gameOver').text(number);
-                        hideButton();
-
-                    }
-                }
-
+                failProcess(game, judgeAnswerString);
             }
+
+        }
     });
 
 });
@@ -54,4 +47,21 @@ function showButton(){
 
     $('.submitButton').show();
     $('.inputPart').show();
+}
+
+function failProcess(game, judgeAnswerString){
+
+    var number = game.modifyNumber();
+    if(number != 'Game over !'){
+        alert('你的答案是:' + judgeAnswerString +'你还有' + number +'次机会!');
+        $('.chanceNum').text(number);
+    }else{
+        $('.chanceNum').text(0);
+        $('.gameOver').text(number);
+        hideButton();
+    }
+}
+function succeedProcess(judgeAnswerString){
+    $('.congratulation').text(judgeAnswerString);
+    hideButton();
 }
